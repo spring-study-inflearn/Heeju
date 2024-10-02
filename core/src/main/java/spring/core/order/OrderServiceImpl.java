33a9f1b71b100a232a98c5c0d5de3f19642f574c
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2024-10-02 23:32:03 +0900

    [section3] 주문과 할인 도메인 개발

---
+++ core/src/main/java/spring/core/order/OrderServiceImpl.java
@@ -0,0 +1,20 @@
+package spring.core.order;
+
+import spring.core.discount.DiscountPolicy;
+import spring.core.discount.FixDiscountPolicy;
+import spring.core.member.Member;
+import spring.core.member.MemberRepository;
+import spring.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        // 단일책임원칙이 잘 지켜진 경우
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
