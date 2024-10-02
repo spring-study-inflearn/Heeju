3da73f03297ecad9a5536520bf923f3f4234f552
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2024-10-02 23:51:03 +0900

    [section4] rateDiscountPolicy 추가 및 testCode

---
+++ core/src/main/java/spring/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,16 @@
+package spring.core.discount;
+
+import spring.core.member.Grade;
+import spring.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+    private int discountPercent = 10;
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP){
+            return price * discountPercent / 100; // 100을 왜 나누지
+        } else {
+            return 0;
+        }
+    }
+}
