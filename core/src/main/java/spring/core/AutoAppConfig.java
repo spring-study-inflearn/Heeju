40470b0f8d6bbc801a721f1a0f5ee0c7be321241
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2024-10-08 04:03:52 +0900

    [section7] annotation만들고, @Filter로 컴포넌트 스캔 대상 설정

--- core/src/main/java/spring/core/AutoAppConfig.java
+++ core/src/main/java/spring/core/AutoAppConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 
 @Component
 @ComponentScan(
@@ -12,8 +13,9 @@ import org.springframework.stereotype.Component;
 /*
 @ComponentScan을 붙이면 @Component가 붙은 클래스를 스캔해서 스프링빈으로 등록
  + @configuration을 붙인 설정정보도 자동으로 등록
-=> 설정정보를 제외ㅏㅎ게 만듦.
+=> 설정정보를 제외하게 만듦.
  */
+
 public class AutoAppConfig {
 
 }
---
+++ core/src/test/java/spring/core/scan/filter/ComponentFilterAppConfigTest.java
@@ -0,0 +1,45 @@
+package spring.core.scan.filter;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.context.annotation.ComponentScan.Filter;
+public class ComponentFilterAppConfigTest {
+
+    @Test
+    void filterScan(){
+        ApplicationContext ac =
+                new AnnotationConfigApplicationContext(ComponentFilterAppConfig.class);
+        BeanA beanA = ac.getBean("beanA", BeanA.class);
+        assertThat(beanA).isNotNull();
+
+//        BeanB beanB = ac.getBean("beanB", BeanB.class);
+//        assertThat(beanB).isNotNull();
+
+        assertThrows(
+                NoSuchBeanDefinitionException.class,
+                () ->ac.getBean("beanB", BeanB.class)
+        );
+
+    }
+
+    @Configuration
+    @ComponentScan(
+            includeFilters = @Filter(type= FilterType.ANNOTATION, classes = MyIncludeComponent.class),
+            excludeFilters = {
+                    @Filter(type=FilterType.ANNOTATION, classes = MyExcludeComponent.class),
+                   // @Filter(type = FilterType.ASSIGNABLE_TYPE, classes = BeanA.class)
+            }
+    )
+    static class ComponentFilterAppConfig{
+
+    }
+}
