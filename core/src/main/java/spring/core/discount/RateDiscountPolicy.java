970f1099a356f2c8751002d482c7a09212783bc4
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2024-10-14 05:11:49 +0900

    [section8] @Primary로 같은 타입 빈 구별하기

--- core/src/main/java/spring/core/discount/RateDiscountPolicy.java
+++ core/src/main/java/spring/core/discount/RateDiscountPolicy.java
@@ -1,11 +1,12 @@
 package spring.core.discount;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import spring.core.member.Grade;
 import spring.core.member.Member;
 @Component
-@Qualifier("mainDiscountPolicy")
+@Primary
 public class RateDiscountPolicy implements DiscountPolicy{
     private int discountPercent = 10;
     @Override
