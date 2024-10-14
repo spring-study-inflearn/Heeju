2c115cf675976b05c8c02adcd96c32ef8ca68e0b
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2024-10-15 04:02:39 +0900

    [section3] servlet 구축 및 HttpRequestHeaderServlet 정보 확인하기

--- mvc1/src/main/java/spring/mvc1/Mvc1Application.java
+++ mvc1/src/main/java/spring/mvc1/Mvc1Application.java
@@ -2,8 +2,10 @@ package spring.mvc1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan // 사블릿 자동등록
 public class Mvc1Application {
 
 	public static void main(String[] args) {
