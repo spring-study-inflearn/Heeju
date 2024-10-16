7af5d3e07448d1e2eb4782a2db2dca90e0419fc2
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2024-10-17 04:10:51 +0900

    [section4] jsp로 회원관리 애플리케이션 만들기 (나중에 설정이나 경로 설정 다시 필요 - 화면 띄우는건 못봄..

--- mvc1/src/main/java/spring/mvc1/Mvc1Application.java
+++ mvc1/src/main/java/spring/mvc1/Mvc1Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
-@ServletComponentScan // 사블릿 자동등록
+@ServletComponentScan // 서블릿 자동등록
 public class Mvc1Application {
 
 	public static void main(String[] args) {
